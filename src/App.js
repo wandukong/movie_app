73d623d56948c0523b34664dfdd1e4e57ff7e14e
wandukong <ysy100476@gmail.com>
2020-08-04 23:19:08 +0900

    State & Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -1,61 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const foodILike = [
-  {
-    id:1,
-    name : "Kimchi",
-    image : "http://www.foodnmed.com/news/photo/201907/18809_4496_540.jpg",
-    rating : 4.5
-  },
-  {
-    id:2,
-    name : "Samhyeopsal",
-    image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=http%3A%2F%2Fcfile30.uf.tistory.com%2Fimage%2F9942B3395A3501C304B135",
-    rating : 5
-  },
-  {
-    id:3,
-    name : "Bibimbap",
-    image: "http://img.etoday.co.kr/pto_db/2019/07/600/20190726153503_1350707_1200_876.jpg",
-    rating : 2
-  },
-  {
-    id:4,
-    name : "Dibcasu",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Original_Tonkatsu.jpg/220px-Original_Tonkatsu.jpg",
-    rating : 4.8
-  },
-  {
-    id:5,
-    name : "Gimbap",
-    image: "https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg",
-    rating : 4.9
+class App extends React.Component{     // class component로 변경한 이유는 state를 사용하기 위해서.
+  state = { // 바꿀 데이터를 넣는다.
+    isLoading : true,
+    movie : []
   }
-]
-
-function Food({name, image, rating}){
-  return <div>
-    <h2>I Like {name}</h2>
-    <h4>{rating}/5.0</h4>
-    <img src= {image} alt={name}/>
-    </div>
+  componentDidMount(){
+    setTimeout(()=>{
+      this.setState({isLoading:false});
+    }, 6000);
+  }
+  render(){ // class component는 render를 가지고 있지 않지만, React component는 render method를 가지고 있기 때문에 사용 가능하다. ->  html code를 보여주게 된다.
+    const {isLoading} = this.state;
+    return (
+      <div>{isLoading? "Loading..." : "we are ready"}</div>
+    );
+  }
 }
 
-Food.propTypes = {        // 원하는 정보가 정확히 오는지에 대한 체크를 위한 PropTypes
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-};
- 
-function App() {   
-  return (
-    <div className="App">
-      {foodILike.map(dish => 
-       (<Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating}/>
-      )) }
-    </div>
-  );
-} // Component : html을 반환하는 함수
+
+// Component Life Cycle
+// mounting 컴포넌트 생성 : construnctor() -> render() -> componentDidMount() 
+// updating 컴포넌트 수정 : setState() 호출되면 -> render() -> componentDidUpdate()
+// unmounting 컴포넌트 제거 : componentWillUnmount()
 
 export default App;
