334b37ace5d3f8f99016e998cd206d50870180f9
wandukong <seungwan37@naver.com>
2020-08-11 00:45:41 +0900

    Updateds CSS

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -7,20 +7,20 @@ const noSummary = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ�
 
 function Movie({year, title, summary, poster, genres}){
     return (
-        <Link to={{
-            pathname:"/movie-datail",
-            state: {year, title, summary, poster, genres}    // state 값을 props로 전달
-        }}>
-            <div className="movie">
+        <div className="movie">
+            <Link to={{
+                pathname:"/movie-datail",
+                state: {year, title, summary, poster, genres}    // state 값을 props로 전달
+                }}>
                 <img src={poster} alt={title} title={title}/>
                 <div className="movie_data">
                     <h3 className="movie_title">{title}</h3>
                     <h5 className="movie_year">{year}</h5>
                     <ul className="movie_genres">{genres.map( (genre, index) => <li key={index} className="genres_genre">{genre}</li>)}</ul>
                     <p className="movie_summary">{!summary ? noSummary:summary.slice(0,200)+"..."}</p>
-                </div>
-            </div>
-        </Link>
+                </div> 
+            </Link>       
+        </div>
     );
 };
 
