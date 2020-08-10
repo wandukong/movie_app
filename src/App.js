a8ec352ae39acddc2783031a7b0ccfa00776d810
wandukong <sungwan37@naver.com>
2020-08-10 21:34:13 +0900

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,54 +1,20 @@
-import React from 'react';
-import axios from "axios"
-import Movie from "./Movie"
+import React from "react"
+import { HashRouter, Route } from "react-router-dom"; /*{라우팅을 위한 컴포넌트, npm install react-router-dom }*/ 
+import Home from "./routes/Home"
+import About from "./routes/About"
+import Detail from "./routes/Detail"
+import Navigation from "./components/Navigation";
 import "./App.css"
 
-class App extends React.Component{     // class component로 변경한 이유는 state를 사용하기 위해서.
-  state = { // 바꿀 데이터를 넣는다.
-    isLoading : true,
-    movies : []
-  };
-
-  getMovies = async () => {               // async와 await는 자바스크립트의 비동기 처리 새로운 패턴. axoios.get()이 끝날 때까지 기다린다.  
-    const {data : {data : {movies}}} = await axios.get("https://yts.mx/api/v2/list_movies.json?sorting_by=rating"); 
-    console.log(movies);
-    this.setState({movies, isLoading : false }) // this.setState({movies:movies}) 단축해서 사용 
-  }
-
-  componentDidMount(){
-    this.getMovies();
-  }
-  render(){ // class component는 render를 가지고 있지 않지만, React component는 render method를 가지고 있기 때문에 사용 가능하다. ->  html code를 보여주게 된다.
-    const {isLoading, movies} = this.state;
-    return (
-      <section className="container">{isLoading? 
-        <div className="loader">
-          <span className="loader_text">Loading...</span>
-        </div> 
-        : 
-        <div className="movies">
-            {movies.map(movie => (
-              <Movie 
-                key={movie.id}
-                id={movie.id}
-                year={movie.year} 
-                title={movie.title} 
-                summary={movie.summary} 
-                poster={movie.medium_cover_image}
-                genres={movie.genres} 
-              />
-            ))}
-        </div>
-      }
-      </section>
-    );
-  }
+function App(){
+  return ( 
+  <HashRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />  {/*exact : 주어진 경로와 정확히 맞아 떨어져야만 라우팅 */}
+    <Route path="/about" component={About} />        
+    <Route path="/movie-datail" component={Detail} />
+  </HashRouter>
+  );
 }
 
-
-// Component Life Cycle
-// mounting 컴포넌트 생성 : construnctor() -> render() -> componentDidMount() 
-// updating 컴포넌트 수정 : setState() 호출되면 -> render() -> componentDidUpdate()
-// unmounting 컴포넌트 제거 : componentWillUnmount()
-
-export default App;
+export default App; 
