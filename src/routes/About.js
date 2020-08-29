1a96fec833c1ec475f98ba3aed0ecd53b8c4b473
wandukong <seungwan37@naver.com>
2020-08-30 02:05:55 +0900

    Updated comment

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,44 +1,10 @@
-import React, {useState, useRef, useEffect} from "react";
+import React from "react";
 import "./About.css"
 
-function useInput(defaultValue){
-    const [value, setValue] = useState(defaultValue);
-
-    const onChange = e => {
-        const {target : {value}} = e;
-        setValue(value);
-    };
-
-    return {value, onChange};
-}
-
-function ClickOutSide(){
-    const ref = useRef(null);
-    const handleClick = e =>{
-        if(ref.current && ref.current.contains(e.target)){
-            alert("이름을 입력하시오.");
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener("click", handleClick)
-    },[]);
-    return ref;
-}
-
-
 function About(){
-    const name = useInput("");
-    const ref = ClickOutSide();
-    console.log(name.value);
     return (
-        <div>
-            <h1>Use Hooks</h1>
-            <br />
-            {/* <input {...name} placeholder="what is your name?" /> 아래와 같음 */} 
-            <div ref={ref}>
-                <input value={name.value} onChange={name.onChange} placeholder="what is your name?" />
-            </div>
+        <div className="about">
+            x
         </div>
     );
 }
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,56 @@
-import React from "react"
+import React, {useMemo, useState, useEffect, useRef, useCallback} from "react"
 import "./Detail.css"
+
+var chk = false;
+
+function useInput(defaultValue){
+    const [value, setValue] = useState(defaultValue);
+
+    const onChange = e => {
+        const {target : {value}} = e;
+        setValue(value);
+    };
+
+    return {value, onChange};
+}
+
+function ClickOutSide(){
+    const ref = useRef(null);
+    const handleClick = e =>{
+        if(!chk && ref.current && ref.current.contains(e.target)){
+            alert("Write your comment. (Maximum characters : 80)");
+            chk = true;
+        }
+    };
+    useEffect(() => {
+        document.addEventListener("click", handleClick)
+    },[]);
+    return ref;
+}
 
 function Detail (props){
     
     const {state} = props.location;
     const {history} = props;
+
+    const [list, setList] = useState([]);
+    const [comment, setComment] = useState('');
+    const ref = ClickOutSide();
+
+    const onChange = useCallback(e=>{
+        setComment(e.target.value);
+    }, []);
+
+    const onInsert = useCallback(e => {
+        if(comment.length === 0){
+            return;
+        }
+        const nextList = list.concat(comment);
+        setList(nextList);
+        setComment('');
+    },[comment, list]);
     
+
     if(state === undefined){
         history.push("/");
     }
@@ -13,7 +58,10 @@ function Detail (props){
     if(state){
         return (
             <div className="detail">
-                <h1 className="detail_title">{state.title} <span className="detail_year">({state.year})</span></h1>     
+                <h1 className="detail_title">{state.title} <span className="detail_year">({state.year})</span></h1>  
+                <div>
+                        
+                </div>   
                 <div className="information">
                     <img src={state.poster}  alt={state.title} title={state.title} style={{width:"250px"}}/>
                     <div>
@@ -31,6 +79,16 @@ function Detail (props){
                 <div className="detail_summary">
                     <h5>Summary</h5>
                     <p>{state.summary}</p>
+                </div>
+                <div className="detail_comment">
+                    <span>Commentㅤ</span>
+                    <input value={comment} onChange={onChange} ref={ref} maxLength="80"/>
+                    <button onClick={onInsert}>📝</button>
+                    <ul>
+                        {list.map((value, index) => (
+                        <li key={index}>{value}</li>
+                        ))}
+                    </ul>
                 </div>
             </div>
         )
